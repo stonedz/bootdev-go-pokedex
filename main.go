9a323758f7f34170e2618a18ee4fd7c033cf445d
Paolo Fagni <paolo.fagni@gmail.com>
2024-06-09 14:09:10 +0200

    added commands help and exit

--- main.go
+++ main.go
@@ -1,8 +1,51 @@
 package main
 
 import "fmt"
+import "bufio"
+import "os"
+
+type cliCommand struct {
+	command     string
+	description string
+	callback    func() error
+}
 
 func main() {
-	fmt.Println("hello world")
+	commands := make(map[string]cliCommand)
+	addCommand(commands, "help", "Prints the help message", commandHelp)
+	addCommand(commands, "exit", "Exits the program", commandExit)
 
+	for {
+		fmt.Print("pokedex >")
+		scanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+		scanner.Scan()
+		text := scanner.Text()
+		handleCommand(text, commands)
+	}
+
+}
+
+func handleCommand(text string, commands map[string]cliCommand) {
+	command, ok := commands[text]
+	if ok {
+		command.callback()
+	} else {
+		fmt.Println("Command not found")
+	}
+
+}
+
+func addCommand(commands map[string]cliCommand, command string, description string, callback func() error) {
+
+	commands[command] = cliCommand{command, description, callback}
+}
+
+func commandHelp() error {
+	fmt.Println("Help message")
+	return nil
+}
+func commandExit() error {
+	fmt.Println("Exiting...")
+	os.Exit(0)
+	return nil
 }
